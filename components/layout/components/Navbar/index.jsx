8a2ea96d5f49656674a0233c33839d4a058adf23
perpetualgrimace <hello@jamesferrell.me>
2023-02-08 17:46:56 -0500

    begins styling Navbar

--- components/layout/components/Navbar/index.jsx
+++ components/layout/components/Navbar/index.jsx
@@ -4,10 +4,6 @@ import { useRouter } from "next/router";
 import { pathPrefix } from "/environment.js";
 import checkRoute from "/helpers/checkRoute";
 
-import NavbarLink from "./NavbarLink";
-import Navmenu from "./Navmenu";
-import NavmenuLink from "./NavmenuLink";
-
 export default function Navbar() {
   const currRoute = useRouter().pathname;
 
@@ -29,38 +25,6 @@ export default function Navbar() {
             />
           </a>
         </Link>
-
-        <ul className="navbar-list">
-          <NavbarLink label="Manage data" href="/manage">
-            <Navmenu>
-              <NavmenuLink href="/manage/jupyterhub">
-                Jupyterhub
-              </NavmenuLink>
-              <NavmenuLink href="/manage/data-integration">
-                Data integration
-              </NavmenuLink>
-              <NavmenuLink href="/manage/business-intelligence">
-                Business intelligence
-              </NavmenuLink>
-            </Navmenu>
-          </NavbarLink>
-
-          <NavbarLink label="Asset viewer" href="/assets">
-            <Navmenu>
-              <NavmenuLink href="/assets/servers">Servers</NavmenuLink>
-              <NavmenuLink href="/assets/data">Data</NavmenuLink>
-              <NavmenuLink href="/assets/engines">Engines</NavmenuLink>
-              <NavmenuLink href="/assets/engagements">
-                Engagements
-              </NavmenuLink>
-            </Navmenu>
-          </NavbarLink>
-
-          <NavbarLink
-            label="Log out"
-            href={`/logout?returnUrl=${currRoute}`}
-          />
-        </ul>
       </nav>
     </div>
   );
