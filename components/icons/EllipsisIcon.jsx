86a58a2b23f0e16a68d9d42f2144614c4b3b1987
perpetualgrimace <hello@jamesferrell.me>
2023-03-09 18:31:36 -0500

    form submission logic (WIP)

---
+++ components/icons/EllipsisIcon.jsx
@@ -0,0 +1,17 @@
+export default function EllipsisIcon() {
+  return (
+    <svg
+      aria-hidden="true"
+      focusable="false"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#707B89"
+        d="M18 10.75a1.25 1.25 0 1 1 0 2.5 1.25 1.25 0 0 1 0-2.5Zm-6 0a1.25 1.25 0 1 1 0 2.5 1.25 1.25 0 0 1 0-2.5Zm-6 0a1.25 1.25 0 1 1 0 2.5 1.25 1.25 0 0 1 0-2.5Z"
+      />
+    </svg>
+  );
+}
--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -6,61 +6,105 @@ import DefaultLayout from "/components/layout/DefaultLayout";
 import Button from "/components/controls/Button";
 import TextField from "/components/controls/TextField";
 
+import EllipsisIcon from "/components/icons/EllipsisIcon";
 import SendIcon from "/components/icons/SendIcon";
+
+const submissionMessage = {
+  success: {
+    title: "Your message was sent",
+    body: "We’ll get back to you shortly",
+  },
+  fail: {
+    title: "Something went wrong",
+    body: "Our apologies. Please email us directly at contact@andalusia.ai",
+  },
+};
+
+let submissionStatus = null;
 
 function onSubmit(values) {
   const { name, email, message } = values;
 
-  console.log(values);
+  console.log(JSON.stringify(values, null, 2));
+
+  if (values) {
+    submissionStatus = "success";
+  } else {
+    submissionStatus = "fail";
+  }
 
   return true;
 }
 
 const TheForm = (props) => {
-  const { touched, errors } = props;
+  const { touched, errors, isSubmitting } = props;
 
   return (
     <DefaultLayout slug="contact">
       <section className="fullscreen-section contact-section">
-        <h1 className="u-text-c u-title">Contact us</h1>
+        <h1 className="u-text-c u-title">
+          {submissionMessage[submissionStatus]?.title || "Contact us"}
+        </h1>
 
-        <Form className="contact-form darkglass u-mh-auto u-mb-md">
-          <TextField
-            labelText="Your name"
-            placeholder="Firstname Lastname"
-            name="name"
-            fontSize="md"
-            touched={touched}
-            errors={errors}
-          />
+        {submissionStatus ? (
+          <div className="contact-form u-mh-auto u-mb-md">
+            <p className="u-font-xl u-text-c">
+              {submissionMessage[submissionStatus]?.body}
+            </p>
+          </div>
+        ) : (
+          <>
+            <Form className="contact-form darkglass u-mh-auto u-mb-md">
+              <TextField
+                labelText="Your name"
+                placeholder="Firstname Lastname"
+                name="name"
+                fontSize="md"
+                touched={touched}
+                errors={errors}
+              />
 
-          <TextField
-            labelText="Email address"
-            placeholder="your@emailaddress.com"
-            name="email"
-            fontSize="md"
-            touched={touched}
-            errors={errors}
-          />
+              <TextField
+                labelText="Email address"
+                placeholder="your@emailaddress.com"
+                name="email"
+                fontSize="md"
+                touched={touched}
+                errors={errors}
+              />
 
-          <TextField
-            labelText="How can we help?"
-            placeholder=" "
-            name="message"
-            fontSize="md"
-            touched={touched}
-            errors={errors}
-            as="textarea"
-          />
+              <TextField
+                labelText="How can we help?"
+                placeholder=" "
+                name="message"
+                fontSize="md"
+                touched={touched}
+                errors={errors}
+                as="textarea"
+              />
 
-          <Button className="contact-form-button" type="submit">
-            <SendIcon /> Send message
-          </Button>
-        </Form>
+              <Button
+                className="contact-form-button"
+                type="submit"
+                disabled={isSubmitting}
+              >
+                {isSubmitting ? (
+                  <>
+                    <EllipsisIcon /> Sending email
+                  </>
+                ) : (
+                  <>
+                    <SendIcon /> Send message
+                  </>
+                )}
+              </Button>
+            </Form>
 
-        <address className="contact-address u-font-sm u-text-c">
-          Dubai Media City, Office 300, Building 5, Dubai, UAE
-        </address>
+            <address className="contact-address u-font-sm u-text-c">
+              Dubai Media City, Office 300, Building 5, Dubai, UAE
+            </address>
+          </>
+        )}
       </section>
     </DefaultLayout>
   );
@@ -78,8 +122,16 @@ const Contact = withFormik({
     email: Yup.string().email("Invalid").required("Required"),
     name: Yup.string().required("Required"),
     message: Yup.string().required("Required"),
+    // email: Yup.string(),
+    // name: Yup.string(),
+    // message: Yup.string(),
   }),
-  handleSubmit: (values) => onSubmit(values),
+  handleSubmit: (values, { setSubmitting }) => {
+    setTimeout(() => {
+      onSubmit(values);
+      setSubmitting(false); // only needed if synchronous
+    }, 1000);
+  },
 })(TheForm);
 
 export default Contact;
